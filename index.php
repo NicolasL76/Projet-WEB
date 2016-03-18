4c452e7342711d28059df92ec8d82218a68e04fc
Antoine <paer763@gmail.com>
2016-03-18 09:15:12 +0100

    Ajout du login

--- index.php
+++ index.php
@@ -6,7 +6,38 @@
     </head>
 
     <body>
-        <?php echo '<p></p>'; ?>
+    <center> <?php echo '
+		<h1>Linkedingue</h1>
+
+		<br/>
+
+		<form action="lol.php" method="post">
+
+			<p>
+
+				<label for="prenom">Ton Prénom:</label>
+				<input type="text" name="prenom" id="prenom" max length="20" placeholder="Entre ton prénom"/><br/>
+
+				<label for="nom">Ton Nom:</label>
+				<input type="text" name="nom" id="nom" max length="20" placeholder="Entre ton nom"/><br/>
+
+				<input type="submit" name="connexion" value="se connecter?"/>
+			</p>
+		</form>
+		<form action="lol.php" method="post">
+
+			<p>
+
+				<label for="prenom">Ton Prénom:</label>
+				<input type="text" name="prenom" id="prenom" max length="20" placeholder="Entre ton prénom"/><br/>
+
+				<label for="nom">Ton Nom:</label>
+				<input type="text" name="nom" id="nom" max length="20" placeholder="Entre ton nom"/><br/>
+
+				<input type="submit" name="connexion" value="se connecter?"/>
+			</p>
+		</form>
+'; ?>	</center>
     </body>
 
 </html>
