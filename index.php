9e208dc00710e29bef7fe4687f19e89d39401b8d
Antoine <paer763@gmail.com>
2016-03-18 09:50:42 +0100

    Ajout du login AVEC CSS

--- index.php
+++ index.php
@@ -7,21 +7,7 @@
     </head>
 
     <body>
-    <?php echo '
-		<h1>Portfolio</h1>
-        <div  class="Login">
-		<form action="Profile.php" method="post">
-
-			<p>
-				<label for="prenom">Votre Identifiant:</label>
-				<input type="text" name="login" id="login" max length="20" placeholder="ex:Saussice"/><br/>
-
-				<label for="nom">Votre Mot de pase:</label>
-				<input type="text" name="mdp" id="mdp" max length="20" placeholder="ex:MAd355"/><br/>
-
-				<input type="submit" name="connexion" value="Lancez Vous!"/>
-			</p>
-		</form></div>'; ?>
+    <?php echo 'A refaire'; ?>
     </body>
 
 </html>
