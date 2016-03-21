64cb632afa7466c6fbb32584d6e8374ac5e86101
Antoine <paer763@gmail.com>
2016-03-21 18:44:03 +0100

    Page d'accueil inscription v6

--- app/views/welcome/Inscription.php
+++ app/views/welcome/Inscription.php
@@ -7,212 +7,181 @@ use Core\Language;
 ?>
 
 
-<html xmlns="http://www.w3.org/1999/html">
+<html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
 
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- feuille de style  -->
-	<link href="css/cover.css" rel="stylesheet">
+    <!-- feuille de style  -->
+    <link href="css/cover.css" rel="stylesheet">
 </head>
 
-<body>
 <header>
-	<div class="logo-wrapper">
-		<div class="site-wrapper-inner">
-			<div class="inner cover">
-				<h1><i>ePortfolio</i></h1>
-				<p>Renseignez les informations ci-dessous et obtenez votre Portfolio personnel!</p>
-			</div>
-		</div>
-	</div>
+    <div class="logo-wrapper">
+        <div class="site-wrapper-inner">
+            <div class="inner cover">
+                <a href="http://projet.web/"><h1><i>ePortfolio</i></h1></a>
+                <p>Renseignez les informations ci-dessous et obtenez votre Portfolio personnel!</p>
+            </div>
+        </div>
+    </div>
 </header>
 <br/>
+
+<body>
 <div class="ultimate-wrapper">
-<div class="row featurette">
-<!-- Module de Profil -->
+    <div class="row featurette">
+        <!-- Module de Profil -->
 
-<div class="col-lg-5">
-	<h1>Profil</h1>
-		<form NAME="Profil">
-			<div>
-				Entrer votre Nom :
-				<input NAME="Nom" TYPE=text SIZE=10 MAXLENGTH=25>
-			</div>
-			<div>
-				Entrez votre Prénom :
-				<input NAME="Prénom" TYPE="text" SIZE="10" MAXLENGTH="25">
-			</div>
-			<div>
-				Entrez votre Adresse :
-				<input NAME="Adresse" TYPE="text" SIZE="25" MAXLENGTH="50">
-			</div>
-			<div>
-				Entrez votre Code postal :
-				<input NAME="CP" TYPE="number" SIZE="5" MAXLENGTH="5">
-			</div>
-			<div>
-				Entrez votre Ville :
-				<input NAME="Ville" TYPE="text" SIZE="10" MAXLENGTH="25">
-			</div>
-			<div>
-				Entrez votre Pays :
-				<input NAME="Pays" TYPE="text" SIZE="10" MAXLENGTH="25">
-			</div>
-			<div>
-				Entrez votre Adresse Mail :
-				<input NAME="Mail" TYPE="text" SIZE="10" MAXLENGTH="255">
-			</div>
-			<div>
-				Entrez votre Numéro de téléphone :
-				<input NAME="Tel" TYPE="number" SIZE="10" MAXLENGTH="15">
-			</div>
-			<div>
-				Entrez votre Date de Naissance :
-				<input NAME="Date_Naissance" TYPE="date" SIZE="10" MAXLENGTH="20">
-			</div>
-			<div>
-				Entrez votre Sexe :
-				<input NAME="Sexe" TYPE="text" SIZE="10" MAXLENGTH="25">
-			</div>
-			<div>
-				Entrez le numéro de votre thème :
-				<input NAME="Thème" TYPE="number" SIZE="10" MAXLENGTH="20">
-			</div>
-		</form>
-</div>
+        <div class="col-lg-5">
+            <h2>Profil</h2>
+            <form NAME="Profil">
+                <fieldset>
+
+                    <label>Entrer votre Nom :</label>
+                    <input NAME="Nom" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+
+                    <label>Entrez votre Prénom :</label>
+                    <input NAME="Prénom" TYPE="text" SIZE="10" MAXLENGTH="25"><br/>
+
+                    <label>Entrez votre Adresse :</label>
+                    <input NAME="Adresse" TYPE="text" SIZE="25" MAXLENGTH="50"><br/>
+
+                    <label>Entrez votre Code postal :</label>
+                    <input NAME="CP" TYPE="number" SIZE="5" MAXLENGTH="5"><br/>
+
+                    <label>Entrez votre Ville :</label>
+                    <input NAME="Ville" TYPE="text" SIZE="10" MAXLENGTH="25"><br/>
 
 
+                    <label>Entrez votre Pays :</label>
+                    <input NAME="Pays" TYPE="text" SIZE="10" MAXLENGTH="25"><br/>
+
+                    <label>Entrez votre Adresse Mail :</label>
+                    <input NAME="Mail" TYPE="text" SIZE="10" MAXLENGTH="255"><br/>
+
+                    <label>Entrez votre Numéro de téléphone :</label>
+                    <input NAME="Tel" TYPE="number" SIZE="10" MAXLENGTH="15"><br/>
+
+                    <label>Entrez votre Date de Naissance :</label>
+                    <input NAME="Date_Naissance" TYPE="date" SIZE="10" MAXLENGTH="20"><br/>
+
+                    <label>Entrez votre Sexe :</label>
+                    <input NAME="Sexe" TYPE="text" SIZE="10" MAXLENGTH="25"><br/>
+
+                    <label>Entrez le numéro de votre thème :</label>
+                    <input NAME="Thème" TYPE="number" SIZE="10" MAXLENGTH="20"><br/>
 
 
-<!-- Module de Formation -->
-<div class="col-lg-5">
-	<h1>Formation</h1>
-		<form NAME="Mes Formations">
-			Entrer votre Année de formation :
-			<input NAME="Annee_Formation" TYPE=text SIZE=10 MAXLENGTH=25>
-			</input>
-			<div>
-				Entrez votre Titre de formation :
-				<input NAME="Titre_Formation" TYPE=text SIZE=10 MAXLENGTH=25>
-				</input>
-			</div>
-			<div>
-				Entrez votre Lieu de Formation :
-				<input NAME="Lieu_Formation" TYPE=text SIZE=10 MAXLENGTH=25>
-				</input>
-			</div>
-			<div>
-				Entrez votre Diplome :
-				<input NAME="Diplome" TYPE=text SIZE=10 MAXLENGTH=25>
-				</input>
-			</div>
-			<div>
-				Entrez le descriptif de votre formation :
-				<div><textearea NAME="Descriptif_Formation" ROWS=4
-								COLS=34>
-					</textearea>
-				</div>
-			</div>
-		</form>
-	</div>
-
-	<!-- Module de Compétence -->
-<div class="col-lg-5">
-	<h1>Compétences</h1>
-		<form NAME="Mes compétences">
-				Catégorie de Compétence :
-			<div>
-			<input NAME="Categorie_Competence" TYPE=text SIZE=10 MAXLENGTH=25> <!-- IMPORTANT : GERER EN PHP  -->
-			</div>
-			<div>
-				Entrer votre Compétence :
-				<input NAME="Competence" TYPE=text SIZE=10 MAXLENGTH=25>
-			</div>
-		</form>
-</div>
-</div>
-	<hr class="featurette-divider"/>
-	<div class="row featurette">
-<!-- Modules de Projet -->
-<div class="col-lg-5">
-	<h1>Projet</h1>
-		<form NAME="Mes Projets">
-			<div>
-				Entrer le nom de votre projet :
-				<input NAME="Nom_Projet" TYPE=text SIZE=10 MAXLENGTH=25>
-			</div>
-			<div>
-				Décrivez votre projet :
-				<div>
-					<textarea NAME="Description_Projet" ROWS=4 COLS=34></textarea>
-				</div>
-			</div>
-			<div>
-				Ajoutez une illustration de votre projet :
-				<input NAME="Lien_Illustration" TYPE=url SIZE=10 MAXLENGTH=200>
-			</div>
-			<div>
-				Ajoutez le lien de la réalisation de votre projet :
-				<input NAME="Lien_Realisation" TYPE=url SIZE=10 MAXLENGTH=200>
-			</div>
-		</form>
-</div>
+        </div>
 
 
-<!-- Module Experience -->
-<div class="col-lg-5">
-		<h1>Experience</h1>
-		<form NAME="Mes Experiences">
-			<div>
-				Entrer vos Années d'éxperiences :
-				<input NAME="Annee_Experience" TYPE=number SIZE=10 MAXLENGTH=2>
-			</div>
-			<div>
-				Entrez votre Poste :
-				<input NAME="Poste" TYPE=text SIZE=10 MAXLENGTH=25>
-			</div>
-			<div>
-				Entrez votre Entreprise :
-				<input NAME="Entreprise" TYPE=text SIZE=10 MAXLENGTH=25>
-			</div>
-			<div>
-				Entrez vos détails de missions :
-				<div>
-					<textarea NAME="Detail_Mission" ROWS=4 COLS=34></textarea>
-				</div>
-			</div>
-			<div>
-				Entrez votre lieu d'éxperience :
-				<input NAME="Lieu_Experience" TYPE=text SIZE=10 MAXLENGTH=25>
-			</div>
-		</form>
-</div>
+        <!-- Module de Formation -->
+        <div class="col-lg-5">
+            <h2>Formation</h2>
+
+            <label>Entrer votre Année de formation :</label>
+            <input NAME="Annee_Formation" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+            </input>
+
+            <label>Entrez votre Titre de formation :</label>
+            <input NAME="Titre_Formation" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+            </input>
+
+            <label>Entrez votre Lieu de Formation :</label>
+            <input NAME="Lieu_Formation" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+            </input>
+
+            <label>Entrez votre Diplome :</label>
+            <input NAME="Diplome" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+            </input>
+
+            <label>Entrez le descriptif de votre formation :</label>
+            <textearea NAME="Descriptif_Formation" ROWS=4 COLS=34/>
+                <br/>
+
+        </div>
+
+        <!-- Module de Compétence -->
+        <div class="col-lg-5">
+            <h2>Compétences</h2>
+            <label>Catégorie de Compétence :</label>
+            <input NAME="Categorie_Competence" TYPE=text SIZE=10 MAXLENGTH=25><br/> <!-- IMPORTANT : GERER EN PHP  -->
+
+            <label>Entrer votre Compétence :</label>
+            <input NAME="Competence" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+
+        </div>
+    </div>
+    <hr class="featurette-divider"/>
+    <div class="row featurette">
+        <!-- Modules de Projet -->
+        <div class="col-lg-5">
+            <h2>Projet</h2>
+
+            <label>Entrer le nom de votre projet :</label>
+            <input NAME="Nom_Projet" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+
+            <label>Décrivez votre projet :</label>
+
+            <textarea NAME="Description_Projet" ROWS=4 COLS=34></textarea><br/>
+
+            <label>Ajoutez une illustration de votre projet :</label>
+            <input NAME="Lien_Illustration" TYPE=url SIZE=10 MAXLENGTH=200><br/>
+
+            <label>Ajoutez le lien de la réalisation de votre projet :</label>
+            <input NAME="Lien_Realisation" TYPE=url SIZE=10 MAXLENGTH=200><br/>
+
+        </div>
 
 
-	<!-- Module thème -->
-	<div class="col-lg-5">
-		<h1>Thème</h1>
-		<form NAME="Mon Thème">
-			<div>
-			Choisissez votre thème de portfolio
-			<a NAME="Choix thème" TYPE=button>
-			<input NAME="Poste" TYPE=button>
-			</div>
-		</form>
+        <!-- Module Experience -->
+        <div class="col-lg-5">
+            <h2>Experience</h2>
 
-	</div>
-</div>
-	<br/>
-	</div>
-	<br/>
+            <label>Entrer vos Années d'éxperiences :</label>
+            <input NAME="Annee_Experience" TYPE=number SIZE=10 MAXLENGTH=2><br/>
+            <label>Entrez votre Poste :</label>
+            <input NAME="Poste" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+            <label>Entrez votre Entreprise :</label>
+            <input NAME="Entreprise" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+            <label>Entrez vos détails de missions :</label>
+            <textarea NAME="Detail_Mission" ROWS=4 COLS=34></textarea><br/>
+            <label>Entrez votre lieu d'éxperience :</label>
+            <input NAME="Lieu_Experience" TYPE=text SIZE=10 MAXLENGTH=25><br/>
+
+
+        </div>
+
+
+        <!-- Module thème -->
+        <div class="col-lg-5">
+            <h2>Thème</h2>
+
+            <div>
+                Choisissez votre thème de portfolio
+                <input NAME="Choix thème" TYPE=button>
+                <input NAME="Poste" TYPE=button>
+            </div>
+
+
+        </div>
+
+        <div class="col-lg-5">
+            <input type="submit" value="submit"/>
+            </fieldset>
+            </form>
+        </div>
+    </div>
+    <hr class="featurette-divider"/>
+    <p>Saussice test inshalla</p>
 </body>
 
+<br/>
 <footer>
-	<p>Copyright &copy; ePortfolio - Saucisse.inc 2016<br/><br/></p>
+    <p>Copyright &copy; ePortfolio - Saucisse.inc 2016<br/><br/></p>
 </footer>
-
 </html>
 
--- app/views/welcome/welcome.php
+++ app/views/welcome/welcome.php
@@ -6,7 +6,7 @@ use Core\Language;
 
 ?>
 
-<html xmlns="http://www.w3.org/1999/html">
+<html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta charset="utf-8">
@@ -14,86 +14,83 @@ use Core\Language;
 
 	<!-- Custom styles for this template -->
 	<link href="css/cover.css" rel="stylesheet">
-
-
+	<!-- Google reCaptcha JS -->
+	<script src="https://www.google.com/recaptcha/api.js"></script>
 </head>
 <header> <div class="logo-wrapper">
 		<div class="site-wrapper-inner">
-				<div class="inner cover">
-					<h1><i>ePortfolio</i></h1>
-					<p><?php echo $data['welcome_message'] ?></p>
-					<p class="lead">
-						<a  class="btn btn-lg btn-default" href="<?php echo DIR; ?>subpage">open subpage
-						</a>
-					</p>
-				</div>
+			<div class="inner cover">
+				<a href="http://projet.web/"><h1><i>ePortfolio</i></h1></a>
+				<p>Créez votre Portfolio en quelques minutes</p>
 			</div>
+		</div>
 	</div>
 </header>
 <body>
-
 <br/>
 <div class="ultimate-wrapper">
 	<div class="row featurette">
 		<h2>Se Connecter</h2>
 		<form action="lol.php" method="post">
-		<div class="col-lg-6">
-			<p>
-				<label for="prenom">Login :</label><br/>
-				<input type="text" name="login" id="login" max length="20" placeholder="Entrez votre prénom"/><br/>
+			<div class="col-lg-6">
+				<p>
+					<label for="prenom">Login :</label><br/>
+					<input type="text" name="login" id="login" max length="20" placeholder="Entrez votre prénom"/><br/>
+					<label for="nom">Mot de passe:</label><br/>
+					<input type="text" name="mdp" id="mdp" max length="20" placeholder="Entrez votre nom"/><br/>
+					<br/>
+				</p>
+			</div>
+			<div class="col-lg-6">
+				<a  class="btn btn-lg btn-warning"  type="submit" name="connexion" value="Lancez vous!" href="<?php echo DIR; ?>editer_portfolio">
+					C'est parti!
+				</a>
 
-				<label for="nom">Mot de passe:</label><br/>
-				<input type="text" name="mdp" id="mdp" max length="20" placeholder="Entrez votre nom"/><br/>
-				<br/>
-
-			</p>
-
-		</div><!-- /.col-lg-4 -->
-		<div class="col-lg-6">
-			<a  class="btn btn-lg btn-warning"  type="submit" name="connexion" value="Lancez vous!" href="<?php echo DIR; ?>editer_portfolio">
-				C'est parti!
-			</a>
-
-		</div>
+			</div>
 	</div>
-</form>
-<hr class="featurette-divider"/>
+	</form>
+	<hr class="featurette-divider"/>
 	<h2>S'inscrire</h2>
-<div class="row featurette">
-	<form action="lol.php" method="post">
-<div class="col-lg-6">
+	<div class="row featurette">
+		<form action="lol.php" method="post">
+			<div class="col-lg-6">
+
+				<fieldset>
 
 
+					<p>
 
-		<p>
+						<label for="prenom">Login :</label><br/>
+						<input type="text" name="login" id="login" max length="20" placeholder="Entrez votre prénom"/><br/>
+						<label for="nom">Email:</label><br/>
+						<input type="text" name="mail" id="mail" max length="20" placeholder="Entrez votre nom"/><br/>
 
-			<label for="prenom">Login :</label><br/>
-			<input type="text" name="login" id="login" max length="20" placeholder="Entrez votre prénom"/><br/>
-			<label for="nom">Email:</label><br/>
-			<input type="text" name="mail" id="mail" max length="20" placeholder="Entrez votre nom"/><br/>
-
-			<label for="nom">Mot de passe:</label><br/>
-			<input type="text" name="mdp" id="mdp" max length="20" placeholder="Entrez votre nom"/><br/>
-			<br/>
-
-		</p>
-</div><!-- /.col-lg-4 -->
-	<div class="col-lg-6">
-		<p>
-			<a  class="btn btn-lg btn-warning"type="submit" name="connexion" value="Lancez vous!" href="<?php echo DIR; ?>Inscription">
-				S'inscrire
-			</a>
-		</p>
-
-	</div><!-- /.col-lg-4 -->
-</div>
-</form>
-<br/>
+						<label for="nom">Mot de passe:</label><br/>
+						<input type="text" name="mdp" id="mdp" max length="20" placeholder="Entrez votre nom"/><br/>
+						<br/>
+						<span class='msg'><?php echo $msg; ?></span>
+					</p>
+			</div>
+			<div class="col-lg-6">
+				<div class="g-recaptcha" data-sitekey="6LfQYhsTAAAAAK5mFRaVoNSLkU6KNjvvaE-y1-nB"></div>
+				<a  class="btn btn-lg btn-warning"type="submit" name="connexion" href="<?php echo DIR; ?>Inscription">
+					S'inscrire
+				</a>
+				</p>
+			</div>
+	</div>
+	</fieldset>
+	</form>
+	<br/>
 </div>
 </body>
 <br/>
 <footer>
 	<p>Copyright &copy; ePortfolio - Saucisse.inc 2016<br/><br/></p>
+	<p class="lead">
+		<a  class="btn btn-lg btn-default" href="<?php echo DIR; ?>subpage">open subpage
+		</a>
+	</p>
 </footer>
 
 </html>
